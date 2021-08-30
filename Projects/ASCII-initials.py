ac221b840952ce8f4d486c355b82921be9b8a2f2
CSI-Rafael-Munoz <24-110@sanignacio.pr>
2021-08-30 09:12:59 -0400

    Update ASCII-initials.py

--- Projects/ASCII-initials.py
+++ Projects/ASCII-initials.py
@@ -1,7 +1,9 @@
-print("""RRRRR            A               M           M              JJJJJ
+print("""
+         RRRRR            A               M           M              JJJJJ
          R   R           A A             M M         M M               J
          R   R          A   A           M   M       M   M              J         
          R R           AAAAAAA         M     M     M     M             J    
          RR           A       A       M       M   M       M        J   J
          R  R        A         A     M         M M         M       J   J    
-         R   R      A           A   M           M           M       JJ             """)      
+         R   R      A           A   M           M           M       JJ             
+         """)      
