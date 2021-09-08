72a341db93a2e7060629f7f29b7caaa19041e6ef
CSI-Rafael-Munoz <24-110@sanignacio.pr>
2021-09-08 09:31:38 -0400

    Script
    Script

---
+++ Modules/Module4/Modulo4.py
@@ -0,0 +1,11 @@
+print("What did you have for breakfast?")
+meal = "Coffee and eggs"
+print(meal)
+
+print("What was your Lunch?")
+meal = "Bistec empanado con arroz y habichuela"
+print(meal)
+
+print("Have you had dinner?")
+meal = "No"
+print(meal)
