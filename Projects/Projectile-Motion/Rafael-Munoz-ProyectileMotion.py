e4cdfc6e03b39a1653314376120688645accfdb1
CSI-Rafael-Munoz <24-110@sanignacio.pr>
2021-11-05 09:16:31 -0400

    Trabajo de Hoy

--- Projects/Projectile-Motion/Rafael-Munoz-ProyectileMotion.py
+++ Projects/Projectile-Motion/Rafael-Munoz-ProyectileMotion.py
@@ -9,9 +9,25 @@
 # gravity_ms = 9.81
 
 import math
-def ProjectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms:int, Building:str, Building_Height:int, gravity_ms:int):
-    time_s = math.sqrt((2 * Building_Height) / gravity_ms)
-    distance_m = (velocity_ms * time_s)
-    print(f"The gun selected for the expirament is {gun}. The caliber of {gun} is {caliber} and the ammunition is {ammunition}. The {gun} is {velocity_ms} fast, and i will be at the {Building} building, which is {Building_Height} tall.")
+def ProjectileFunction(experimentalData: ExperimentalData):
+    time_s = math.sqrt((2 * experimentalData.Building_Height) / experimentalData.gravity_ms)
 
-ProjectileFunction("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M61", 826, "Soleil", 288, 9.81)
+    distance_m = (experimentalData.velocity_ms * time_s)
+    # distance_m = (experimentalData[velocity_ms] * time_s)
+    print(f"The gun selected for the expirament is {experimentalData.gun}. The caliber of {experimentalData.gun} is {experimentalData.caliber} and the ammunition is {experimentalData.ammunition}. The {experimentalData.gun} is {experimentalData.velocity_ms} fast, and i will be at the {experimentalData.Building} building, which is {experimentalData.Building_Height} tall.")
+
+#Convert your script parameters into a single JSON object
+
+# experimentalData = {
+#     "gun" : "M700 Bolt action Sniper",
+#  "caliber" : "7.62 x 51mm NATO",
+#  "ammunition" :"M61",
+#  "velocity_ms" : 826,
+#  "Building" : "Soleil",
+#  "Building_Height" : 288,
+#  "gravity_ms" : 9.81
+
+# }
+experimentalData = ExperimetnalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M61", 826, "Soleil", 288, 9.81)
+
+ProjectileFunction
---
+++ Projects/Projectile-Motion/experimentalData.py
@@ -0,0 +1,10 @@
+class ExperimentalData:
+    def __init__(self, gun:str, caliber:str, ammunition:str, velocity_ms:int, Building:str, Building_Height:int, gravity_ms:int):
+        self.gun = gun
+        self.caliber = caliber
+        self.ammunition = ammunition
+        self.velocity_ms = velocity_ms
+        self.Building = Building
+        self.Building_Height = Building_Height
+        self.gravity_ms = gravity_ms
+        
