a1bfe01d633a9783723c8aa930803656f26483ae
CSI-Rafael-Munoz <24-110@sanignacio.pr>
2021-11-09 09:29:30 -0400

    Update Rafael-Munoz-ProyectileMotion.py

--- Projects/Projectile-Motion/Rafael-Munoz-ProyectileMotion.py
+++ Projects/Projectile-Motion/Rafael-Munoz-ProyectileMotion.py
@@ -1,3 +1,9 @@
+import math
+import os
+from pathlib import Path
+from ExperimentalData import ExperimentalData
+import json
+
 # gun = "M700 Bolt action Sniper"
 # caliber = "7.62 x 51mm NATO"
 # ammunition = "M61"
@@ -8,17 +14,16 @@
 
 # gravity_ms = 9.81
 
-import math
 def ProjectileFunction(experimentalData: ExperimentalData):
     time_s = math.sqrt((2 * experimentalData.Building_Height) / experimentalData.gravity_ms)
 
     distance_m = (experimentalData.velocity_ms * time_s)
     # distance_m = (experimentalData[velocity_ms] * time_s)
-    print(f"The gun selected for the expirament is {experimentalData.gun}. The caliber of {experimentalData.gun} is {experimentalData.caliber} and the ammunition is {experimentalData.ammunition}. The {experimentalData.gun} is {experimentalData.velocity_ms} fast, and i will be at the {experimentalData.Building} building, which is {experimentalData.Building_Height} tall.")
+    print(f"The gun selected for the experiment is {experimentalData.gun}. The caliber of {experimentalData.gun} is {experimentalData.caliber} and the ammunition is {experimentalData.ammunition}. The {experimentalData.gun} is {experimentalData.velocity_ms} fast, and i will be at the {experimentalData.Building} building, which is {experimentalData.Building_Height} tall, and it will reach {experimentalData.distance_m} in {experimentalData.time_s}.")
 
 #Convert your script parameters into a single JSON object
 
-# experimentalData = {
+# experimentalData = { 
 #     "gun" : "M700 Bolt action Sniper",
 #  "caliber" : "7.62 x 51mm NATO",
 #  "ammunition" :"M61",
@@ -28,6 +33,25 @@ def ProjectileFunction(experimentalData: ExperimentalData):
 #  "gravity_ms" : 9.81
 
 # }
-experimentalData = ExperimetnalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M61", 826, "Soleil", 288, 9.81)
+experimentalData = ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M61", 826, "Soleil", 288, 9.81)
 
-ProjectileFunction
+myDataSet = [
+ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M61", 826, "Soleil", 288, 9.81),
+ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M62", 816, "Soleil", 288, 9.81),
+ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M80", 833, "Soleil", 288, 9.81),
+ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "M993", 910, "Soleil", 288, 9.81),    
+ExperimentalData("M700 Bolt action Sniper", "7.62 x 51mm NATO", "Ultra Nosler", 822, "Soleil", 288, 9.81),
+]
+
+for data in myDataSet:
+    print("\n---------------------------------------------------------------------\n")
+    ProjectileFunction(data)
+
+#Serialization
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, "Proyectile-Motion.json")
+
+print(myOutputFilePath)
+
+with open(myOutputFilePath, "w") as outfile:
+    json.dump([data.__dict__for data in myDataSet], outfile)
